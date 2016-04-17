5617fa557daf4397965b072b81f66d95008eace7
U-ARGONET\ahb5 <ahb5@JCPL-2SB2.argo.uwf.edu>
2016-04-16 19:24:58 -0500

    Started the framework for the main menu

--- CProject/UWFSSD/MainMenu.c
+++ CProject/UWFSSD/MainMenu.c
@@ -4,12 +4,116 @@
  * and open the template in the editor.
  */
 
+//Level- 1 patient, 2 nurse, 3 doc, 4 admin
+
+
 
 #include <stdio.h>
-void displayMainMenu(){
+void displayMainMenu(int level, int remind){
     
+   
+    level =4;
+    remind =0;
+
+   int choice =0;
+   
+
+  do{  
+
     printf("WELCOME TO THE UWF SSD SECURE MEDICAL RECORDS \n");
-    printf("1: do things");
     
-    
+    if(level == 1){//nurse
+        printf("[1] View personal records\n");
+        printf("[2] Edit personal info\n");
+	printf("[9] Exit Menu \n");
+    	
+	if(remind == 0){
+        	printf("You have no reminders\n");
+    	}else{
+        //Reminders funct
+    	}
+	scanf(" %d", &choice);
+    	switch(choice){
+	    case 1://View pers rec funct
+            break;
+            case 2://Edit personal info funct
+            break;
+	    case 9:
+	        printf("Exiting menu \n");
+		break;
+	    default:
+	        printf("Invalid input\n");
+	    break;
+    	}	
+    }else if(level ==2){//nurse
+        printf("[1] View Patient records\n");
+	printf("[2] Update Patient records \n");
+        printf("[3] Record vitals\n"); 
+        printf("[9] Exit menu \n");
+	scanf(" %d", &choice);
+
+	switch(choice){
+	    case 1://View patients info
+	    break;
+	    case 2://view update patient records 
+	    break;
+	    case 3://Record vitals funct
+	    break;
+	    case 9://exit
+	        printf("Exiting menu \n");
+		break;
+	    default:
+	        printf("Invalid input");
+	    break;
+	}
+    }else if(level == 3){//Doc
+        printf("[1] View Patient records\n");
+	printf("[2] Update Patient records \n");
+	printf("[3] Medical Reconciliation \n");
+	printf("[4] Transfer of care \n");
+	printf("[9] Exit menu \n");
+
+	scanf(" %d", &choice);
+
+	switch(choice){
+	    case 1://View patients info
+	    break;
+	    case 2://Update rec
+	    break;
+	    case 3://Medical rec
+	    break;
+	    case 4://Transfer 
+	    break;
+	    case 9://exit
+	        printf("Exiting \n");
+		break;
+	    default:
+	        printf("Invalid input\n");
+	    break;
+
+	}
+
+    }else if(level == 4){//admin
+
+	printf("[1] Set up new User account \n");
+	printf("[2] Deactivate User account \n");
+	printf("[9] Exit menu \n");
+        scanf(" %d", &choice);
+
+
+	switch(choice){
+	    case 1://Setup user
+	    break;
+	    case 2://remove user
+	    break;
+	    case 9:
+	        printf("Exiting menu \n");
+		break;
+	    default:
+	        printf("Invalid input \n");
+		break;
+	}
+
+    }
+  }while(choice != 9);   
 }
