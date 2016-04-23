c931ea398b259ba9b88e77d6568928f31c129ec7
Ben <bjc9001@gmail.com>
2016-04-23 17:06:06 -0500

    fixing more stupid shit

--- CProject/UWFSSD/main.c
+++ CProject/UWFSSD/main.c
@@ -21,7 +21,7 @@
  * 
  */
 int main(int argc, char** argv) {
-    LoginT login = loginMain()
+    LoginT login = loginMain();
     
    if(login!=NULL)
         displayMainMenu(login.permissionLevel,0,login.username,login.uuid);
